fe39a7baf6b4bf76022aaf500c47e9c6b2d99052
Murilo <murilommms@gmail.com>
2019-07-05 14:45:20 -0300

    Adding automatic inserts into table

---
+++ meetingwizard/src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+insert into MEETING_ROOM (ROOM_NAME) values ('Alemanha');
+insert into MEETING_ROOM (ROOM_NAME) values ('Turquia');
+insert into MEETING_ROOM (ROOM_NAME) values ('Irlanda');
+insert into MEETING_ROOM (ROOM_NAME) values ('Australia');
