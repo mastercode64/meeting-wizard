5ee75db97e25981ac9b4f53a14cb9f9b4bdfb0eb
Murilo <murilommms@gmail.com>
2019-07-05 11:27:33 -0300

    Committing back end

---
+++ meetingwizard/src/main/kotlin/com/wirecard/meetingwizard/MeetingwizardApplication.kt
@@ -0,0 +1,11 @@
+package com.wirecard.meetingwizard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetingwizardApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetingwizardApplication>(*args)
+}
