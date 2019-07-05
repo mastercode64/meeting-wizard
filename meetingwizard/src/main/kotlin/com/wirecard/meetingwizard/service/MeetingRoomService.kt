8a77e3a5c0b1e463b8f1da6bbc0c99eb8dabae88
Murilo <murilommms@gmail.com>
2019-07-05 14:20:20 -0300

    Correcting file names

--- meetingwizard/src/main/kotlin/com/wirecard/meetingwizard/service/MeetingRoomService.kt
+++ meetingwizard/src/main/kotlin/com/wirecard/meetingwizard/service/MeetingRoomService.kt
@@ -1,16 +1,16 @@
 package com.wirecard.meetingwizard.service
 
-import com.wirecard.meetingwizard.repository.MeetingRepository
+import com.wirecard.meetingwizard.repository.MeetingRoomRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class MeetingService {
+class MeetingRoomService {
     @Autowired
-    lateinit var meetingRepository: MeetingRepository
+    lateinit var meetingRoomRepository: MeetingRoomRepository
 
     fun listMeetingRooms() =
-            meetingRepository.findAll()
+            meetingRoomRepository.findAll()
 }
 
 
