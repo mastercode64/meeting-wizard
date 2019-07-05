ce1527223e90f2bae677ea2c369654b5926f76d1
Murilo <murilommms@gmail.com>
2019-07-05 13:53:00 -0300

    Adding repository and service classes

---
+++ meetingwizard/src/main/kotlin/com/wirecard/meetingwizard/service/MeetingService.kt
@@ -0,0 +1,14 @@
+package com.wirecard.meetingwizard.service
+
+import com.wirecard.meetingwizard.repository.MeetingRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class MeetingService
+
+    @Autowired
+    lateinit var meetingRepository: MeetingRepository
+
+    fun listMeetingRooms() =
+            meetingRepository.findAll()
