4c42512c3a19421f0caa1015b0e214dbecf39bd9
LeonardoLB <leonardolsbufalo@gmail.com>
2019-07-05 15:56:32 -0300

    update files

--- front/src/tools/server.js
+++ front/src/tools/server.js
@@ -6,6 +6,12 @@ var fileServer = new nStatic.Server('../../../front');
 
 http.createServer(function (req, res) {
     
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
     fileServer.serve(req, res);
+
 
 }).listen(3000);
