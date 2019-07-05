3c1a94d1bc550a9198c187aa2a22803642543da7
mastercode64 <marcosfujihashi@gmail.com>
2019-07-05 15:50:42 -0300

    Adicionado usuarios ficticios

--- meetingwizard/src/main/resources/data-h2.sql
+++ meetingwizard/src/main/resources/data-h2.sql
@@ -2,3 +2,10 @@ insert into MEETING_ROOM (ROOM_NAME) values ('Alemanha');
 insert into MEETING_ROOM (ROOM_NAME) values ('Turquia');
 insert into MEETING_ROOM (ROOM_NAME) values ('Irlanda');
 insert into MEETING_ROOM (ROOM_NAME) values ('Australia');
+
+INSERT INTO PERSON (name) VALUES ('Maria');
+INSERT INTO PERSON (name) VALUES ('João');
+INSERT INTO PERSON (name) VALUES ('Pedro');
+INSERT INTO PERSON (name) VALUES ('Marcos');
+INSERT INTO PERSON (name) VALUES ('Leonardo');
+INSERT INTO PERSON (name) VALUES ('Eduardo');
