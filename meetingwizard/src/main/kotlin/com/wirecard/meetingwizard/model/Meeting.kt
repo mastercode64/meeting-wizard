ae840eb66505c5606dd4a9ca5daef14fe8a7b0cd
mastercode64 <marcosfujihashi@gmail.com>
2019-07-05 12:32:35 -0300

    Adicionado entidade 'sala de reuniao'

---
+++ meetingwizard/src/main/kotlin/com/wirecard/meetingwizard/model/Meeting.kt
@@ -0,0 +1,14 @@
+package com.wirecard.meetingwizard.model
+
+import javax.persistence.*
+
+
+@Entity
+data class MeetingRoom(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long,
+    val roomName: String
+
+)
+
